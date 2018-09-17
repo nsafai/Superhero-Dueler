a5c7629e41b0934a705a485ef0927f1a14ef4294
Nicolai Safai <nicolaisafai@gmail.com>
2018-09-17 16:54:16 -0700

    finished defining ability and hero class init

--- superheroes.py
+++ superheroes.py
@@ -1,27 +1,58 @@
 import random
 
+
 class Ability:
     def __init__(self, name, attack_strength):
         # Set Ability name
+        self.name = name
         # Set attack strength
+        self.attack_strength = attack_strength
 
     def attack(self):
-        # Return attack value
+
+        # Calculate lowest attack value as an integer. "should be half of highest possible attack value"
+        lowest_attack_value = self.attack_strength // 2
+
+        # Use random.randint(a, b) to select a random attack value.
+        attack_value = random.randint(lowest_attack_value, self.attack_strength)
+
+
+        # Return attack value between 0 and the full attack.
+        return attack_value
 
     def update_attack(self, attack_strength):
         # Update attack value
+        self.attack_strength = attack_strength
 
 
 class Hero:
     def __init__(self, name):
         # Initialize starting values
+        self.abilities = list()
+        self.name = name
 
     def add_ability(self, ability):
         # Add ability to abilities list
+        self.abilities.append(ability)
 
     def attack(self):
         # Run attack() on every ability hero has
+        total_damage = 0
+
+        for ability in self.abilities:
+            # ability.attack()
+            total_damage += ability.attack()
+
+        return total_damage
 
 
 if __name__ == "__main__":
     # If you run this file from the terminal this block is executed.
+    hero = Hero("Wonder Woman")
+    print(hero.attack())
+    ability = Ability("Divine Speed", 300)
+    hero.add_ability(ability)
+    print(hero.attack())
+    new_ability = Ability("Super Human Strength", 800)
+    hero.add_ability(new_ability)
+    print(hero.attack())
