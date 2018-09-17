29f4a2ae7424f2de85d17c028b0c2c5f36baa0cf
Nicolai Safai <nicolaisafai@gmail.com>
2018-09-17 16:00:05 -0700

    skeleton of code

---
+++ README.md
@@ -0,0 +1,4 @@
+# Building a superhero dueler
+Using Python, follow this [tutorial](https://www.makeschool.com/academy/track/superhero-team-dueler/superhero-team-dueler/abilihero)
+
+To run, clone the repo, open a terminal, make sure you have python 3 installed and type `python3 superheroes.py`
---
+++ superheroes.py
@@ -0,0 +1,27 @@
+import random
+
+class Ability:
+    def __init__(self, name, attack_strength):
+        # Set Ability name
+        # Set attack strength
+
+    def attack(self):
+        # Return attack value
+
+    def update_attack(self, attack_strength):
+        # Update attack value
+
+
+class Hero:
+    def __init__(self, name):
+        # Initialize starting values
+
+    def add_ability(self, ability):
+        # Add ability to abilities list
+
+    def attack(self):
+        # Run attack() on every ability hero has
+
+
+if __name__ == "__main__":
+    # If you run this file from the terminal this block is executed.
