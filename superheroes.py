56db64629a0e3149b9dc1594fa8f4ad42131f70a
Nicolai Safai <nicolaisafai@gmail.com>
2018-09-19 13:53:00 -0700

    added testing

--- superheroes.py
+++ superheroes.py
@@ -1,6 +1,5 @@
 import random
 
-
 class Ability:
     def __init__(self, name, attack_strength):
         # Set Ability name
@@ -16,14 +15,12 @@ class Ability:
         # Use random.randint(a, b) to select a random attack value.
         attack_value = random.randint(lowest_attack_value, self.attack_strength)
 
-
         # Return attack value between 0 and the full attack.
         return attack_value
 
     def update_attack(self, attack_strength):
         # Update attack value
         self.attack_strength = attack_strength
-
 
 class Hero:
     def __init__(self, name):
@@ -45,6 +42,44 @@ class Hero:
 
         return total_damage
 
+class Weapon(Ability):
+    def attack(self):
+        weapon_attack_value = random.randint(0, self.attack_strength)
+
+        return weapon_attack_value
+        # return a random value between 0 and the full attack power of the weapon. Hint: The attack power is inherited.
+
+class Team:
+    def init(self, team_name):
+        # Instantiate resources
+        self.name = team_name
+        self.heroes = list()
+
+    def add_hero(self, Hero):
+        # Add Hero object to heroes list
+        self.heroes.append(Hero)
+
+    def remove_hero(self, name):
+        # Remove hero from heroes list.
+        # If Hero isn't found return 0.
+        for hero in self.heroes:
+            if hero.name == name:
+                self.heroes.remove(hero)
+            else:
+                return 0
+
+    def find_hero(self, name):
+        # Find and return hero from heroes list.
+        # If Hero isn't found return 0.
+        for hero in self.heroes:
+            return hero
+        else:
+            return 0
+
+    def view_all_heroes(self):
+        # Print out all heroes to the console.
+        for hero in self.heroes:
+            print hero
 
 if __name__ == "__main__":
     # If you run this file from the terminal this block is executed.
